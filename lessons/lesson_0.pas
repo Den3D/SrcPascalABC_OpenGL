22bfa71f3b9a7b9afabfabc810ea1481e7301532
TheMrDen3D <den16081988@mail.ru>
2017-09-17 20:15:17 +0300

    Добавлены коды 0 и 1 урока

---
+++ lessons/lesson_0.pas
@@ -0,0 +1,34 @@
+ {$reference Tao.FreeGlut.dll}
+ {$reference Tao.OpenGl.dll}
+
+   
+ uses
+   System, System.Collections.Generic, System.Linq, System.Text, 
+   Tao.OpenGl, Tao.FreeGlut;
+
+
+ procedure init_graphics;
+ begin
+
+ end;
+
+ procedure on_display();
+ begin
+
+ end;
+
+ procedure on_reshape( w, h:integer);
+ begin
+
+ end;
+
+ 
+ begin
+   Glut.glutInit();
+   Glut.glutInitWindowSize(500, 500);
+   Glut.glutCreateWindow('Tao Example');
+   init_graphics();
+   Glut.glutDisplayFunc(on_display);
+   Glut.glutReshapeFunc(on_reshape);
+   Glut.glutMainLoop();
+ end.
---
+++ lessons/lesson_1.pas
@@ -0,0 +1,38 @@
+﻿{$reference Tao.FreeGlut.dll}
+{$reference Tao.OpenGl.dll}
+   
+uses
+   System, System.Collections.Generic, System.Linq, System.Text, 
+   Tao.OpenGl, Tao.FreeGlut;
+
+// Инициализация ресурсов приложения и OpenGL
+ procedure InitScene;
+ begin
+  Writeln( GL.glGetString(GL.GL_VERSION));
+ end;
+
+// Процедура отрисовки
+// Данная процедура вызывается каждый кадр
+ procedure RenderScene();
+ begin
+ end;
+
+// Процедура перенастройки
+// Данная процедура вызывается при изменении размера экрана
+ procedure Reshape( w, h:integer);
+ begin
+ end;
+
+ 
+ begin
+  Glut.glutInit(); 
+  Glut.glutInitWindowSize(400, 300);
+  Glut.glutInitWindowPosition(300, 200); 
+  Glut.glutInitDisplayMode(GLUT.GLUT_RGBA);
+  Glut.glutCreateWindow('Tao Example');  
+  Glut.glutDisplayFunc(RenderScene);
+  Glut.glutReshapeFunc(Reshape);
+  
+  InitScene();
+  Glut.glutMainLoop();
+ end.
